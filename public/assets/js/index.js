4e4423e78599cf8554c0de5415ea4450d50f2d00
twopheat <twopheat@gmail.com>
2020-03-03 23:41:42 -0800

    fixing

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,182 @@
-$(document).ready(function(){var n,r;function t(t,o,e){o.setPosition(e),o.setContent(t?"Error: The Geolocation service failed.":"Error: Your browser doesn't support geolocation."),o.open(n)}function o(){$.get("/api/CatchHistory",function(t){console.log(t);for(var o=0;o<t.length;o++){console.log(t[o].lat);var e=$("<div>").addClass("catchList").attr("id","fish-catchList-"+o);$("#fishCatch").append(e).append(t[o].fish_type);var a=$("<div>").addClass("catchList").attr("id","rig-catchList-"+o);$("#rigCatch").append(a).append(t[o].rig_type);var i=$("<div>").addClass("catchList").attr("id","time-catchList-"+o);$("#timeCatch").append(i).append(t[o].createdAt)}})}console.log("ready"),$(document).on("click","#fishLocationSubmitButton",function(t){t.stopPropagation(),navigator.geolocation.getCurrentPosition(function(t){var o={lat:t.coords.latitude,lng:t.coords.longitude};console.log(o);var e=$("#fish_type").val().trim(),a=$("#rig_type").val().trim();console.log(e);new google.maps.Marker({position:o,map:n,icon:"http://maps.google.com/mapfiles/kml/shapes/fishing.png"});n.setCenter(this.getPosition()),r.setPosition(this.getPosition()),r.setContent("{USER_HERE} has caught: ${fishType}!"),r.open(n,this);var i={fish_type:e,lat:parseFloat(t.coords.latitude),lng:parseFloat(t.coords.longitude),rig_type:a};console.log(i),$.post("/api/CatchHistory",i).then(function(t){console.log(t),location.reload()})}),$("#fishCatch").html(""),$("#rigCatch").html(""),$("#timeCatch").html(""),o()}),$(document).on("submit","#rigSubmitBtn",function(t){t.preventDefault();var o={rig_name:$("#rig_name").val().trim(),rod:$("#rod").val().trim(),reel:$("#reel").val().trim(),tackle:$("#tackle").val().trim(),info:$("#info").val().trim()};$.post("/api/Rig",o).then(function(t){console.log("Rig data: "+t),location.reload()}),$("#rig_name").val(""),$("#rod").val(""),$("#reel").val(""),$("#tackle").val(""),$("#info").val("")}),n=new google.maps.Map(document.getElementById("googleMap"),{center:{lat:0,lng:0},zoom:12}),r=new google.maps.InfoWindow,navigator.geolocation?navigator.geolocation.getCurrentPosition(function(t){var o={lat:t.coords.latitude,lng:t.coords.longitude};n.setCenter(o)},function(){t(!0,r,n.getCenter())}):t(!1,r,n.getCenter()),o()});
+$(document).ready(function () {
+  console.log("ready");
+
+  // Event listeners
+  $(document).on("click", "#fishLocationSubmitButton", fishDataSubmit);
+  $(document).on("submit", "#rigSubmitBtn", rigDataSubmit);
+
+  var map, infoWindow;
+  map = new google.maps.Map(document.getElementById("googleMap"), {
+    center: {
+      lat: 0,
+      lng: 0
+    },
+    zoom: 12
+  });
+  infoWindow = new google.maps.InfoWindow;
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      map.setCenter(pos);
+    }, function () {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  };
+  
+  function fishDataSubmit(event) {
+    event.stopPropagation();
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+      // get CURRENT location
+      var currentPosition = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      console.log(currentPosition);
+      // Get information for the form and add a popup tip window
+      // Use jquery to grab the form element and get the data
+      //var fishType = "PLACEHOLDER";
+      var fishType = $("#fish_type").val().trim();
+      var rig_type = $("#rig_type").val().trim();
+
+      console.log(fishType);
+
+
+      // icon link 
+      var image = "http://maps.google.com/mapfiles/kml/shapes/fishing.png";
+
+      // create a new google Maps marker
+      var newMarker = new google.maps.Marker({
+        position: currentPosition,
+        map: map,
+        icon: image
+      });
+        map.setCenter(this.getPosition())
+        infoWindow.setPosition(this.getPosition());
+        infoWindow.setContent(`{USER_HERE} has caught: ${fishType}!`);
+        infoWindow.open(map, this);
+
+
+      
+
+      /// Need help with POST problem is probably in the api-routes //////////
+      var catchData = {
+        fish_type: fishType,
+        lat: parseFloat(position.coords.latitude),
+        lng: parseFloat(position.coords.longitude),
+        rig_type: rig_type
+      };
+      
+      console.log(catchData);
+      //Send the POST for newCatch to db 
+      $.post("/api/CatchHistory", catchData)
+      .then(function(data) {
+          console.log(data);
+          location.reload();
+        });
+      });
+      $("#fishCatch").html("");
+      $("#rigCatch").html("");
+      $("#timeCatch").html("");
+      getCatches();
+    }
+    getCatches();
+
+  function getCatches(){
+    $.get("/api/CatchHistory", function(data){
+      console.log(data);
+      
+
+
+      // for each catch that our server sends us back
+    for (var i = 0; i < data.length; i++) {
+      console.log(data[i].lat);
+
+      var fishCatchDiv = $("<div>").addClass("catchList").attr("id", "fish-catchList-" + i);
+        $("#fishCatch").append(fishCatchDiv).append(data[i].fish_type);
+
+      var rigCatchDiv = $("<div>").addClass("catchList").attr("id", "rig-catchList-" + i);
+      $("#rigCatch").append(rigCatchDiv).append(data[i].rig_type);
+
+        var timeCatchDiv = $("<div>").addClass("catchList").attr("id", "time-catchList-" + i);
+        $("#timeCatch").append(timeCatchDiv).append(data[i].createdAt);
+
+        // navigator.geolocation.getCurrentPosition(function () {
+        //   // get CURRENT location
+          
+          
+        //   var currentPosition = {
+        //     lat: data[i].lat,
+        //     lng: data[i].lng
+        //   };
+  
+        //   var fishType = data[i].fish_type;
+  
+        //   //console.log(fishType);
+  
+        //     // icon link 
+        //   var image = "http://maps.google.com/mapfiles/kml/shapes/fishing.png";
+  
+        //   // create a new google Maps marker
+        //   var newMarker = new google.maps.Marker({
+        //     position: currentPosition,
+        //     map: map,
+        //     icon: image
+        //   });
+        //     map.setCenter(this.getPosition())
+        //     infoWindow.setPosition(this.getPosition());
+        //     infoWindow.setContent(`{USER_HERE} has caught: ${fishType}!`);
+        //     infoWindow.open(map, this);
+        // })
+      } // end for loop
+    
+    })// end GET
+  
+  } //end function getCatches(){
+
+
+  
+
+  function rigDataSubmit(event){
+    event.preventDefault();
+
+    //Make a new rig
+    var newRig = {
+      rig_name: $("#rig_name").val().trim(),
+      rod: $("#rod").val().trim(),
+      reel: $("#reel").val().trim(),
+      tackle: $("#tackle").val().trim(),
+      info: $("#info").val().trim()
+    };
+
+    $.post("/api/Rig", newRig)
+    .then(function(data){
+      console.log("Rig data: "+ data);
+      location.reload();
+    });
+
+    //empty each input box by replacing the value with an empty string
+    $("#rig_name").val("");
+    $("#rod").val("");
+    $("#reel").val("");
+    $("#tackle").val("");
+    $("#info").val("");
+  }
+
+})
+
